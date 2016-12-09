5a8b7f2b2e3f76d61f761dd0f474540828ef1fdf
parabola949 <parabola949@gmail.com>
2016-12-09 14:54:03 -0600

    Very quick initial setup with reply

---
+++ CoupForTelegram/CoupForTelegram/Bot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace CoupForTelegram
+{
+    internal static class Bot
+    {
+        internal static TelegramBotClient Api;
+        internal static void Initialize()
+        {
+            var key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64)
+                        .OpenSubKey("SOFTWARE\\Coup");
+#if DEBUG
+            var TelegramAPIKey = key.GetValue("DebugAPI").ToString();
+#elif RELEASE
+            var TelegramAPIKey = key.GetValue("ProductionAPI").ToString();
+#endif
+            Api = new TelegramBotClient(TelegramAPIKey);
+            Api.OnMessage += ApiOnOnMessage;
+            Api.StartReceiving();
+        }
+
+        private static void ApiOnOnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            Api.SendTextMessageAsync(messageEventArgs.Message.Chat.Id, "I'm alive!!");
+        }
+    }
+}
---
+++ CoupForTelegram/CoupForTelegram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace CoupForTelegram
+{
+    class Program
+    {
+        internal static string RootDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        
+        static void Main(string[] args)
+        {
+            new Thread(Bot.Initialize).Start();
+            Thread.Sleep(-1);
+        }
+
+        
+    }
+}
