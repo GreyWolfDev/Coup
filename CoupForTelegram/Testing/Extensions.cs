6a74bb7da7a37fffbf66ee66e5ea2445f56a1941
Kuvam <kuvam@users.noreply.github.com>
2017-02-21 07:54:21 +0530

    Add files via upload

--- CoupForTelegram/Testing/Extensions.cs
+++ CoupForTelegram/Testing/Extensions.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Testing
 {
@@ -11,15 +7,10 @@ namespace Testing
     {
         public static void Shuffle<T>(this IList<T> list)
         {
-            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
             int n = list.Count;
             while (n > 1)
             {
-                byte[] box = new byte[1];
-                do provider.GetBytes(box);
-                while (!(box[0] < n * (Byte.MaxValue / n)));
-                int k = (box[0] % n);
-                n--;
+                int k = (new Random()).Next(n--);
                 T value = list[k];
                 list[k] = list[n];
                 list[n] = value;
